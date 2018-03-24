6e030f770120f150fbe00a19c8aa05e352380665
Utkarsh Prasad <uprasad96@gmail.com>
2018-03-24 20:51:25 +0530

    Screen Capture

--- ssReader.py
+++ ssReader.py
@@ -1,3 +1,10 @@
 import cv2
-image = cv2.imread('SS/ss.jpg')
-cv2.imwrite('ss.jpg', image)
+import numpy as np
+import pyautogui
+image = pyautogui.screenshot()
+# image = cv2.imread('ss.jpg')
+image = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+crop_img = image[350:1050, 280:780]
+cv2.imshow("cropped", crop_img)
+cv2.waitKey(0)
+# cv2.imwrite('ss.jpg', crop_img)
